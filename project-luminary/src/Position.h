7c1b955bb3b72a3988f7838339f3d0be46f371ce
MB <xburian@mendelu.cz>
2023-11-23 16:10:20 +0100

    Code update - Position creation

---
+++ project-luminary/src/Position.h
@@ -0,0 +1,13 @@
+//
+// Created by Martin on 23.11.2023.
+//
+
+#ifndef PROJECT_LUMINARY_POSITION_H
+#define PROJECT_LUMINARY_POSITION_H
+
+struct Position{
+    unsigned short x;
+    unsigned short y;
+};
+
+#endif //PROJECT_LUMINARY_POSITION_H
