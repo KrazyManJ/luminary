5c5b5d550d815a45cbc12f9dbe1054c64e0368a7
KrazyManJ <KrazyManJ@gmail.com>
2023-11-29 00:25:36 +0100

    Updated code - added ability to go to other maps, added little debug thing, updated map rendering via MapObjects

--- project-luminary/src/Game.cpp
+++ project-luminary/src/Game.cpp
@@ -6,50 +6,69 @@ void Game::render() {
     ConsoleHandler::setCursorPosition(1, 1);
     std::cout << ConsoleHandler::getFormatChar(ConsoleHandler::RESET);
     std::cout << m_mapMatrix.at(m_currentMapPos.y).at(m_currentMapPos.x)->render() << std::endl;
+    ConsoleHandler::setCursorPosition(m_player->getPosition());
+    std::cout << m_player->renderChar();
+
+    //DEBUG TOOLS
+    ConsoleHandler::setCursorPosition(2, Window::HEIGHT+2);
     std::cout << "Player pos - x: "
         << m_player->getPosition().x << "   y: "
         << m_player->getPosition().y << "      ";
-    ConsoleHandler::setCursorPosition(m_player->getPosition());
-    std::cout << m_player->renderChar();
+    for (unsigned int i = 0; i < m_mapMatrix.size(); i++) {
+        ConsoleHandler::setCursorPosition(Window::WIDTH+2, i+1);
+        for (unsigned int j = 0; j < m_mapMatrix.at(i).size(); j++){
+            if (m_mapMatrix.at(i).at(j) == nullptr) std::cout << " ";
+            else if (i == m_currentMapPos.y && j == m_currentMapPos.x) std::cout << "x";
+            else std::cout << "-";
+        }
+    }
 }
 
 void Game::onInput(ConsoleHandler::KeyEvent *evt) {
     if (!evt->isArrowEscaped()) return;
-    // PLAYER MOVEMENT
     Position pPos = m_player->getPosition();
-    if (evt->getKey() == KEY_ARROW_RIGHT && pPos.x < WIDTH) m_player->makeMovement(RIGHT);
-    else if (evt->getKey() == KEY_ARROW_LEFT && pPos.x > 1) m_player->makeMovement(LEFT);
-    else if (evt->getKey() == KEY_ARROW_UP && pPos.y > 1) m_player->makeMovement(UP);
-    else if (evt->getKey() == KEY_ARROW_DOWN && pPos.y < HEIGHT) m_player->makeMovement(DOWN);
-
-
-    // MAP SWITCH TEST
-//    if (
-//            evt->getKey() == KEY_ARROW_UP
-//            && m_currentMapPos.y > 0
-//            && m_mapMatrix.at(m_currentMapPos.y - 1).size() > m_currentMapPos.x
-//            && m_mapMatrix.at(m_currentMapPos.y - 1).at(m_currentMapPos.x) != nullptr
-//            )
-//        m_currentMapPos.y -= 1;
-//    else if (
-//            evt->getKey() == KEY_ARROW_DOWN
-//            && m_currentMapPos.y < m_mapMatrix.size() - 1
-//            && m_mapMatrix.at(m_currentMapPos.y + 1).size() > m_currentMapPos.x
-//            && m_mapMatrix.at(m_currentMapPos.y + 1).at(m_currentMapPos.x) != nullptr
-//            )
-//        m_currentMapPos.y += 1;
-//    else if (
-//            evt->getKey() == KEY_ARROW_LEFT
-//            && m_currentMapPos.x > 0
-//            && m_mapMatrix.at(m_currentMapPos.y).at(m_currentMapPos.x - 1) != nullptr
-//            )
-//        m_currentMapPos.x -= 1;
-//    else if (
-//            evt->getKey() == KEY_ARROW_RIGHT
-//            && m_currentMapPos.x < m_mapMatrix.at(m_currentMapPos.y).size()-1
-//            && m_mapMatrix.at(m_currentMapPos.y).at(m_currentMapPos.x + 1) != nullptr
-//            )
-//        m_currentMapPos.x += 1;
+    if (evt->getKey() == KEY_ARROW_RIGHT) {
+        if (pPos.x < WIDTH) {
+            m_player->makeMovement(RIGHT);
+            return;
+        }
+        if (m_currentMapPos.x >= m_mapMatrix.at(m_currentMapPos.y).size()-1) return;
+        if (m_mapMatrix.at(m_currentMapPos.y).at(m_currentMapPos.x + 1) == nullptr) return;
+        m_player->setPosition({.x=1,.y=pPos.y});
+        m_currentMapPos.x += 1;
+    }
+    else if (evt->getKey() == KEY_ARROW_LEFT) {
+        if (pPos.x > 1){
+            m_player->makeMovement(LEFT);
+            return;
+        }
+        if (m_currentMapPos.x <= 0) return;
+        if (m_mapMatrix.at(m_currentMapPos.y).at(m_currentMapPos.x - 1) == nullptr) return;
+        m_player->setPosition({.x=Window::WIDTH,.y=pPos.y});
+        m_currentMapPos.x -= 1;
+    }
+    else if (evt->getKey() == KEY_ARROW_UP) {
+        if (pPos.y > 1){
+            m_player->makeMovement(UP);
+            return;
+        }
+        if (m_currentMapPos.y <= 0) return;
+        if (m_mapMatrix.at(m_currentMapPos.y - 1).size() <= m_currentMapPos.x) return;
+        if (m_mapMatrix.at(m_currentMapPos.y - 1).at(m_currentMapPos.x) == nullptr) return;
+        m_player->setPosition({.x=pPos.x,.y=Window::HEIGHT});
+        m_currentMapPos.y -= 1;
+    }
+    else if (evt->getKey() == KEY_ARROW_DOWN) {
+        if (pPos.y < HEIGHT){
+            m_player->makeMovement(DOWN);
+            return;
+        }
+        if (m_currentMapPos.y >= m_mapMatrix.size() - 1) return;
+        if (m_mapMatrix.at(m_currentMapPos.y + 1).size() <= m_currentMapPos.x) return;
+        if (m_mapMatrix.at(m_currentMapPos.y + 1).at(m_currentMapPos.x) == nullptr) return;
+        m_player->setPosition({.x=pPos.x,.y=1});
+        m_currentMapPos.y += 1;
+    }
 }
 
 Game *Game::debugGame() {
@@ -58,44 +77,60 @@ Game *Game::debugGame() {
             {.x=10,.y=10},
             new CharData('*',0xFFFF00,COLOR_NONE)
     );
+    const std::string emptyMatrix =
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "WWGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGWWWWWWWWWWGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "WWWWWWWWWWWWGGGGGGGGGGGGGGGGGGGGGGGGGGGGWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "WWWWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGWWWWWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGGG\n"
+            "GGWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGWWWWWWWWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGWWWWWWWWWWWWWGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"
+            "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n";
+    std::map<char,std::function<MapObject*()>> map = {
+            {
+                'G', []() { return new MapObject(
+                    new CharData(' ',COLOR_NONE, 0x1a472e),
+                    new CharData(' ',COLOR_NONE, 0x1a472e),
+                    false
+                    ); }
+            },
+            {
+                'W', []() { return new MapObject(
+                        new CharData(' ',COLOR_NONE, 0x253352),
+                        new CharData(' ',COLOR_NONE, 0x253352),
+                        false
+                ); }
+            }
+    };
     game->m_mapMatrix = {
             {
-                    new Map(
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            "                                                                                \n"
-                            ),
-                    new Map("-x\n--\n--"),
+                    new Map(emptyMatrix,map),
+                    new Map(emptyMatrix,map),
             },
             {
-                    new Map("--\nx-\n--"),
-                    new Map("--\n-x\n--"),
+                    new Map(emptyMatrix,map),
+                    new Map(emptyMatrix,map),
             },
             {
-                    new Map("--\n--\nx-"),
-                    new Map("--\n--\n-x"),
+                    new Map(emptyMatrix,map),
+                    new Map(emptyMatrix,map),
             }
     };
     game->m_currentMapPos = {.x = 0, .y=0};
