b0a80e201902a68990fc883f6235b11ab1759e79
xlysek1 <xlysek1>
2023-11-29 17:56:31 +0100

    Updated code - added Heal class

--- project-luminary/src/Heal.cpp
+++ project-luminary/src/Heal.cpp
@@ -1,1 +1,3 @@
 #include "Heal.h"
+
+
--- project-luminary/src/Heal.h
+++ project-luminary/src/Heal.h
@@ -1,9 +1,16 @@
 #ifndef PROJECT_LUMINARY_HEAL_H
 #define PROJECT_LUMINARY_HEAL_H
 
+#include "Item.h"
+#include "CharRenderable.h"
 
-class Heal {
+class Heal: public Item {
+private:
+    float m_healValue;
+public:
+    Heal(std::string name,float m_healValue, CharData* charData );
 
+    float getHealValue();
 };
 
 
