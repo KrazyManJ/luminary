133c516f617622fffe7440ccfeb068e457d3c03f
MB <xburian@mendelu.cz>
2023-11-29 17:55:32 +0100

    Code updated - Enemy implemented and UML diagram updated - ItemEntity and InteractiveObject modified

--- project-luminary/src/Enemy.cpp
+++ project-luminary/src/Enemy.cpp
@@ -1,17 +1,18 @@
 #include "Enemy.h"
 
-Enemy::Enemy(Position position, int health, unsigned int damage, CharData *charData) {
-
+Enemy::Enemy(Position position, int health, unsigned int damage, CharData *charData) : InteractiveObject(position, charData) {
+    m_health = health;
+    m_damage = damage;
 }
 
 int Enemy::getHealth() {
-    return 0;
+    return m_health;
 }
 
 unsigned int Enemy::getDamage() {
-    return 0;
+    return m_damage;
 }
 
 void Enemy::dealDamage(unsigned int incomingDamage) {
-
+    m_health -= incomingDamage;
 }
