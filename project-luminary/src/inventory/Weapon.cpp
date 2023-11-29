679db0da7ced909ae273a22a0b321afba7365178
xlysek1 <xlysek1>
2023-11-29 18:51:56 +0100

    Updated code - added Weapon class

--- project-luminary/src/inventory/Weapon.cpp
+++ project-luminary/src/inventory/Weapon.cpp
@@ -1,5 +1,10 @@
-//
-// Created by Martin on 26.11.2023.
-//
-
 #include "Weapon.h"
+
+
+Weapon::Weapon(std::string name, float damageValue, CharData *charData) : Item(name, charData) {
+    m_damageValue = damageValue;
+}
+
+float Weapon::getDamage() {
+    return m_damageValue;
+}
--- project-luminary/src/inventory/Weapon.h
+++ project-luminary/src/inventory/Weapon.h
@@ -1,9 +1,15 @@
 #ifndef PROJECT_LUMINARY_WEAPON_H
 #define PROJECT_LUMINARY_WEAPON_H
 
+#include "Item.h"
 
-class Weapon {
+class Weapon : public Item{
+private:
+    float m_damageValue;
+public:
+    Weapon(std::string name, float m_damageValue,CharData* charData );
 
+    float getDamage();
 };
 
 
