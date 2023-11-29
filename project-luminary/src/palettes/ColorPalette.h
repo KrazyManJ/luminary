730f4699c7dd65d7eb4fb6b52e42c938ac1dbdac
xplesova <xplesova@mendelu.cz>
2023-11-29 16:21:02 +0100

    Updated code - added ColorPalette class

---
+++ project-luminary/src/palettes/ColorPalette.h
@@ -0,0 +1,10 @@
+
+#ifndef PROJECT_LUMINARY_COLORPALETTE_H
+#define PROJECT_LUMINARY_COLORPALETTE_H
+
+class ColorPalette {
+    static const unsigned int FG_PLAYER = 0xFCF8BC;
+    static const unsigned int BG_PLAYER = 0xDB600D;
+};
+
+#endif //PROJECT_LUMINARY_COLORPALETTE_H
