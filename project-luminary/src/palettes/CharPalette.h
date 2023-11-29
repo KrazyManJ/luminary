eb00ffc6b8dfb4a09abee1aa80429d66dbd6e6a7
xplesova <xplesova@mendelu.cz>
2023-11-29 16:33:13 +0100

    Updated code - added CharPalette class

---
+++ project-luminary/src/palettes/CharPalette.h
@@ -0,0 +1,9 @@
+
+#ifndef PROJECT_LUMINARY_CHARPALETTE_H
+#define PROJECT_LUMINARY_CHARPALETTE_H
+
+class CharPalette {
+    static const char PLAYER = '*';
+};
+
+#endif //PROJECT_LUMINARY_CHARPALETTE_H
