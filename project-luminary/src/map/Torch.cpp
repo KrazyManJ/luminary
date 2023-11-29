63385bef10950fa3cdde72410f57ba75a3d12e19
xlysek1 <xlysek1>
2023-11-29 19:38:53 +0100

    Updated code - added Torch class

---
+++ project-luminary/src/map/Torch.cpp
@@ -0,0 +1,10 @@
+#include "Torch.h"
+
+Torch::Torch(CharData *charData1, CharData *lightCharData1, bool isObstacle, CharData *charData,
+             CharData *lightCharData) : MapObject(charData1, lightCharData1, isObstacle) {
+
+}
+
+void Torch::lightUp() {
+
+}
