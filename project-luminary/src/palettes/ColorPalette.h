34b77ce78b61da6e3ce222a2bea971bcd4fd447c
xplesova <xplesova@mendelu.cz>
2023-12-03 15:18:37 +0100

    Updated code - map updated

--- project-luminary/src/palettes/ColorPalette.h
+++ project-luminary/src/palettes/ColorPalette.h
@@ -11,6 +11,14 @@ public:
     static const unsigned int LIGHT_BLUE = 0x007FBF;
     static const unsigned int DARK_GREEN = 0x006464;
     static const unsigned int GREEN = 0x008D00;
+    //light map
+    static const unsigned int DAY_GREEN = 0x80FF80;
+    static const unsigned int DAY_BLUE = 0x38A8AB;
+    static const unsigned int LIGHT_GREY = 0xC0C0C0;
+    static const unsigned int DARK_GREY = 0x808080;
+    static const unsigned int PATH = 0xD0B968;
+    static const unsigned int YELLOW = 0xFFFFB3;
+    static const unsigned int ORANGE = 0xFF8040;
 };
 
 #endif //PROJECT_LUMINARY_COLORPALETTE_H
