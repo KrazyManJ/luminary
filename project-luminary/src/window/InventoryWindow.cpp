5055e0924f696aef754d13de69d492f3fb9a28ed
xlysek1 <xlysek1>
2024-01-19 15:31:05 +0100

    Updated code - updated inventorywindow, added grid to inventory

--- project-luminary/src/window/InventoryWindow.cpp
+++ project-luminary/src/window/InventoryWindow.cpp
@@ -5,25 +5,8 @@ void InventoryWindow::render() {
 
     const unsigned short MARGIN_WIDTH = 4;
     const unsigned short MARGIN_HEIGHT = 2;
-    const unsigned short GRID_HEIGHT = 5;
-    const unsigned short GRID_WIDTH = 3;
-//col=columm
-    /*for (unsigned int row = MARGIN_HEIGHT; row < Window::HEIGHT - MARGIN_HEIGHT; row++) {
-        ConsoleHandler::setCursorPosition(MARGIN_WIDTH + 1, row + 1);
-        std::string output;
-        for (unsigned int col = MARGIN_WIDTH; col < Window::WIDTH - MARGIN_WIDTH; col++) {
-            output.append(ConsoleHandler::getFormatChar(RESET) + " ");
-        }
-        std::cout << output;
-    }
-     */
-    const std::string INVENTORY_LABEL = "Inventory";
-    ConsoleHandler::setCursorPosition(Window::WIDTH / 2 - INVENTORY_LABEL.length() / 2, MARGIN_HEIGHT + 1);
-    std::cout << INVENTORY_LABEL;
-
-
-//tady zacina muj kodik:3
-
+    const unsigned short GRID_HEIGHT = 2;
+    const unsigned short GRID_WIDTH = 4;
 
     for (unsigned int row = MARGIN_HEIGHT; row < Window::HEIGHT - MARGIN_HEIGHT; row++) {
         ConsoleHandler::setCursorPosition(MARGIN_WIDTH + 1, row + 1);
@@ -35,28 +18,28 @@ void InventoryWindow::render() {
 
     }
 
-
-    for (unsigned int row = MARGIN_HEIGHT; row < Window::HEIGHT - MARGIN_HEIGHT; row += GRID_HEIGHT + 1) {
-        for (unsigned int col = MARGIN_WIDTH; col < Window::WIDTH - MARGIN_WIDTH; col += GRID_WIDTH + 1) {
-            ConsoleHandler::setCursorPosition(col + 1, row + 1);
-
-
-            std::cout << ConsoleHandler::getFormatChar(RESET) << "+-----+";
-            ConsoleHandler::setCursorPosition(col + 1, row + 3);
-            std::cout << ConsoleHandler::getFormatChar(RESET) << "|     |";
-            ConsoleHandler::setCursorPosition(col + 1, row + 3);
-            std::cout << ConsoleHandler::getFormatChar(RESET) << "+-----+";
-
+    for (unsigned int gridH = MARGIN_HEIGHT + 4; gridH < Window::HEIGHT - MARGIN_HEIGHT - 2; gridH++) {
+        ConsoleHandler::setCursorPosition(MARGIN_WIDTH + 4, gridH + 1);
+        std::string output;
+        for (unsigned int gridW = MARGIN_WIDTH + 4; gridW < Window::WIDTH / 2; gridW += 2) {
+            if (gridH % 2 == 0)
+                output.append(ConsoleHandler::getFormatChar(RESET) + "| ");
+            else
+                output.append(ConsoleHandler::getFormatChar(RESET) + "--");
         }
+        output.append(ConsoleHandler::getFormatChar(RESET) + (gridH % 2 == 0 ? "|" : "-"));
+        std::cout << output;
     }
+
+
+    const std::string INVENTORY_LABEL = "Inventory";
+    ConsoleHandler::setCursorPosition(Window::WIDTH / 2 - INVENTORY_LABEL.length() / 2, MARGIN_HEIGHT + 1);
+    std::cout << INVENTORY_LABEL;
 
 }
 
-
-//tady snad kocni:,)
-
 void InventoryWindow::onInput(ConsoleHandler::KeyEvent *evt) {
-    if (evt->getKey()== KEY_E|| evt->getKey()== KEY_ESC) close();
+    if (evt->getKey() == KEY_E || evt->getKey() == KEY_ESC) close();
 }
 
 InventoryWindow::InventoryWindow(Window *prevWindow, Inventory *inventory) : ReturnableWindow(prevWindow) {
