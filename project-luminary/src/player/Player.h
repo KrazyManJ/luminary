985f6bfedb27cfb5714ce3e5b250c8b98b34a3b0
mbbk13 <burian.martin@stud.vassboskovice.cz>
2024-01-26 22:07:50 +0100

    Revert "Code updated - one getDamage() renamed and bug repaired"
    This reverts commit 49fd0e9fa1d2093ba0aa237f8aceec43ef0e1bc0.

--- project-luminary/src/player/Player.h
+++ project-luminary/src/player/Player.h
@@ -29,7 +29,7 @@ public:
     PlayerAttack** getAttacks();
     float getHealth();
     int getDamage(PlayerAttack* playerAttack);
-    float getDamageAvg();
+    float getDamage();
     ~Player(); //v ramci smazani hrace vymaze i inventar a utoky z pameti
 };
 
