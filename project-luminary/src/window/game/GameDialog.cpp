5200af60f109e975a011090c81d1a517cdc0a4bf
KrazyManJ <KrazyManJ@gmail.com>
2024-01-28 16:19:49 +0100

    Updated Code - Enhanced Game Dialog

--- project-luminary/src/window/game/GameDialog.cpp
+++ project-luminary/src/window/game/GameDialog.cpp
@@ -3,29 +3,37 @@
 #include "GameDialog.h"
 
 void GameDialog::render() {
-    for (int i = 20; i < 25; i++){
+    const short Y_DIALOG_POS = m_game->getPlayer()->getPosition().y < Window::HEIGHT/2 ? 19 : 2;
+    for (int i = Y_DIALOG_POS; i < Y_DIALOG_POS+6; i++){
         ConsoleHandler::setCursorPosition(2,i);
         std::cout
             << ConsoleHandler::getFormatChar(RESET)
             << std::string(Window::WIDTH-2,' ');
     }
-    ConsoleHandler::setCursorPosition(4,22);
+    ConsoleHandler::setCursorPosition(4,Y_DIALOG_POS+2);
     if (m_faded) {
         std::cout << m_text;
-        return;
+
+    } else {
+        for (char c : m_text){
+            std::cout << c;
+            usleep(4000);
+        }
+        m_faded = true;
     }
-    for (char c : m_text){
-        std::cout << c;
-        usleep(5000);
-    }
-    m_faded = true;
+    ConsoleHandler::setCursorPosition(70,Y_DIALOG_POS+4);
+    std::cout
+        << ConsoleHandler::getFormatChar(BLINKING)
+        << ConsoleHandler::getFormatChar(INVERTED)
+        << " Enter ";
 }
 
 void GameDialog::onInput(ConsoleHandler::KeyEvent *evt) {
     if (evt->getKey() == KEY_ENTER) close(true);
 }
 
-GameDialog::GameDialog(std::string text, Window *prevWindow) : ReturnableWindow(prevWindow) {
+GameDialog::GameDialog(std::string text, Game *game) : ReturnableWindow(game) {
     m_faded = false;
     m_text = text;
+    m_game = game;
 }
--- project-luminary/src/window/game/GameDialog.h
+++ project-luminary/src/window/game/GameDialog.h
@@ -3,13 +3,15 @@
 
 
 #include "../ReturnableWindow.h"
+#include "../../Game.h"
 
 class GameDialog : public ReturnableWindow {
 private:
     bool m_faded;
     std::string m_text;
+    Game* m_game;
 public:
-    GameDialog(std::string text, Window *prevWindow);
+    GameDialog(std::string text, Game *game);
 
     void render() override;
 
