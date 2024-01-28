30310ad46db0188fc7df12a2b6b3bed15a31568e
MB <xburian@mendelu.cz>
2024-01-28 16:45:28 +0100

    Code updated - after pickup dialog showing now

--- project-luminary/src/map/ItemEntity.cpp
+++ project-luminary/src/map/ItemEntity.cpp
@@ -1,5 +1,7 @@
 #include "ItemEntity.h"
 #include "../Game.h"
+#include "../Luminary.h"
+#include "../window/game/GameItemPickupDialogWindow.h"
 
 
 ItemEntity::ItemEntity(Item *item, Position position, CharData *charData) : InteractiveObject(position, charData) {
@@ -30,8 +32,10 @@ void ItemEntity::onPlayerProximity(Game *game) {}
 void ItemEntity::onPlayerEnter(Game *game) {
     Inventory* playersInventory = game->getPlayer()->getInventory();
     if(dynamic_cast<Heal*>(m_item) != nullptr){
-        playersInventory->addHeal(dynamic_cast<Heal*>(pickUp()));
+        playersInventory->addHeal(dynamic_cast<Heal*>(m_item));
+        Luminary::getInstance()->openWindow(new GameItemPickupDialogWindow(game,pickUp()), true);
     }else{
-        playersInventory->addWeapon(dynamic_cast<Weapon*>(pickUp()));
+        playersInventory->addWeapon(dynamic_cast<Weapon*>(m_item));
+        Luminary::getInstance()->openWindow(new GameItemPickupDialogWindow(game,pickUp()), true);
     }
 }
